c58906e502c57dc18435b0bea047432055e0b9ea
rtrwslv <83144402+rtrwslv@users.noreply.github.com>
2024-11-16 00:32:43 +0300

    first commit

--- load_balancer/app.py
+++ load_balancer/app.py
@@ -1,11 +1,7 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, send_from_directory, jsonify
 
 app = Flask(__name__)
-
-# Конфигурации сервисов
-rpc_gateway_url = "http://rpc_gateway:5001"
-static_gateway_url = "http://static_gateway:80"
+STATIC_FOLDER = "./static_gateway/static"
 
 @app.route('/rpc', methods=['POST'])
 def rpc():
@@ -15,8 +11,10 @@ def rpc():
 
 @app.route('/static', methods=['GET'])
 def static_content():
-    response = requests.get(static_gateway_url + "/static")
-    return response.content, response.status_code
+    try:
+        return send_from_directory(STATIC_FOLDER, "main.jpg")
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"}), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- rpc_gateway/app.py
+++ rpc_gateway/app.py
@@ -1,20 +1,52 @@
 from flask import Flask, request, jsonify
-import uuid
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+
+METHODS = {
+    'summ': lambda args: {"result": sum(args)},
+    'multiply': lambda args: {"result": prod(args)}
+}
+
+def prod(args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
 @app.route('/rpc', methods=['POST'])
 def rpc():
-    data = request.json
-    method = data['method']
-    
-    if method == 'summ':
-        return jsonify(summ(data['data']['args']))
-    else:
-        return jsonify({"error": "Method not found"}), 404
+    try:
+        if not request.is_json:
+            raise ValueError("Request must be JSON")
 
-def summ(args):
-    return {"result": sum(args)}
+        data = request.json
+
+        if 'method' not in data or 'data' not in data or 'args' not in data['data']:
+            raise KeyError("Missing 'method' or 'data' or 'args' fields")
+
+        method = data['method']
+        args = data['data']['args']
+
+        if not isinstance(args, list) or not all(isinstance(x, (int, float)) for x in args):
+            raise TypeError("'args' must be a list of numbers")
+
+        if method in METHODS:
+            return jsonify(METHODS[method](args))
+        else:
+            return jsonify({"error": f"Method '{method}' not found"}), 404
+
+    except KeyError as e:
+        return jsonify({"error": str(e)}), 400
+    except TypeError as e:
+        return jsonify({"error": str(e)}), 400
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        app.logger.error(f"Unexpected error: {e}")
+        return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
